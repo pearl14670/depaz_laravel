8e974a0e8d1ce59cb44dadee0e26b76caaf615a9
pearl14670 <pearlguro48@gmail.com>
2025-04-28 19:34:40 -0700

    roles and User seeder update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->enum('role',  ['admin', 'user'])->default( 'user');
-            $table->enum('status',  ['1','0'])->default('1');
+            $table->unsignedBigInteger('role_id');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::insert(
+            [
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('111'),
+                'role_id' => 1,
+
+            ],
+            [
+                'name' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('111'),
+                'role_id' => 2,
+            ],
+        ]
+    );
+
+    }
+
+}
